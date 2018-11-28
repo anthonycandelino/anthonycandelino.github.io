84a8d83156871611789d69e48e4e32c794ab2651
Anthony Candelino <anthony.candelino@tulip.com>
2018-11-27 23:15:11 -0500

    minor edits

--- personal-website/js/script.js
+++ personal-website/js/script.js
@@ -14,9 +14,9 @@ $(document).ready(function() {
     // });
 
     var typedSubTitles = new Typed(".typedSubTitles", {
-        strings: ["Computer Science Student", "University of Guelph", "Co-op Computer Science Student"],
-        typeSpeed: 80,
-        // loop: true,
+        strings: ["Computer Science Student", "University of Guelph",],
+        typeSpeed: 85,
+        loop: true,
         startDelay: 1000,
         showCursor: false,
     });
